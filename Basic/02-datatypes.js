49502eeac86c62cfcef8f53ec7c7280fafaf9522
JareedStx <javierjgc170@gmail.com>
2025-02-27 10:17:22 -0600

    4to Commit

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,29 @@
+/*Tipos de datos primitivos dentro de JavaScript */
+
+//String (Cadena de texto)
+let name = "Javier Jared"
+let alias ='Japi'
+
+//Números   (number)
+
+let age = 37
+let height = 1.67
+
+// Booleanos (boolean)
+let isStudent = true
+let isTeacher = false
+
+// Undefined    No tiene un valor concreto
+let UndefinedValue
+console.log(UndefinedValue)
+
+//Null
+let nullValue = null
+console.log(nullValue)
+
+//Symbol    Representa valores únicos, como identificadores de propiedades.
+let mySymbol = Symbol("mySymbol")
+
+//BigInt    Número enteros extremandamente grande, ocupa la memoria necesaria y tiene las siguientes dos formas de representación
+let myBigInt = BigInt(191816516874169816816816846716846816846168681681978615)
+let myBigInt2 = 191816516874169816816816846716846816846168681681978615n
