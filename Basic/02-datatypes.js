ff6e78e02eabb08d057b91af33295f686e024015
JareedStx <javierjgc170@gmail.com>
2025-02-27 21:45:42 -0600

    5to Commit

--- Basic/02-datatypes.js
+++ Basic/02-datatypes.js
@@ -1,7 +1,7 @@
 /*Tipos de datos primitivos dentro de JavaScript */
 
 //String (Cadena de texto)
-let name = "Javier Jared"
+let myname = "Javier Jared"
 let alias ='Japi'
 
 //Números   (number)
@@ -27,3 +27,22 @@ let mySymbol = Symbol("mySymbol")
 //BigInt    Número enteros extremandamente grande, ocupa la memoria necesaria y tiene las siguientes dos formas de representación
 let myBigInt = BigInt(191816516874169816816816846716846816846168681681978615)
 let myBigInt2 = 191816516874169816816816846716846816846168681681978615n
+
+//Mostramos los tipos de datos
+console.log(typeof myname)
+console.log(typeof alias)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+
+console.log(typeof UndefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
