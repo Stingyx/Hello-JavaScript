ed80ab816dfe1bef5838e5481e1385e9cf5ee6e8
JareedStx <javierjgc170@gmail.com>
2025-03-01 13:12:47 -0600

    Leccion 06

---
+++ Basic/06-strings.js
@@ -0,0 +1,35 @@
+//Strings
+
+let myname = "Japi"
+let greeting ="Hola, " + myname
+console.log(greeting)
+console.log(typeof greeting)
+
+//Longitud Cadenas de texto
+console.log(greeting.length)
+
+//Acceso a caracteres
+console.log(greeting[0])
+console.log(greeting[9])
+
+//Métodos comunes 
+console.log(greeting.toUpperCase())
+console.log(greeting.toLowerCase())
+console.log(greeting.indexOf("Japi"))
+console.log(greeting.indexOf("Hola"))
+console.log(greeting.includes("Japi"))
+console.log(greeting.includes("Hola"))
+console.log(greeting.includes("Buenas"))
+console.log(greeting.slice(0,8))
+console.log(greeting.replace("Japi", "Javier"))
+
+let mensaje = `Hola,
+esta es mi
+prueba
+de
+mensaje en
+varias
+lineas
+utilizando alt+96 
+para acento invertido `
+console.log(mensaje)
